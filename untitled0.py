01577479102296ab16f980b516c6aa311f72870b
李范红 <vivi3n@126.com>
2017-01-28 21:32:08 +0800

    something changes

--- untitled0.py
+++ untitled0.py
@@ -8,7 +8,11 @@ Created on Wed Jan 25 00:30:48 2017
 
 print('hello world')
 print('I like 6.00.1x')
+#in the following 3 expreesions, the word[-1]
+#repersents the last char of the string and
+#when the gap is -1 it starts at word[-1]
 print('')
 print('abcdefgh'[::-1])
 print('abcdefgh'[:-1:-1])
 print('abcdefgh'[:-1])
+print('abcdefgh'[:-2:-1])
