4b66c773805e0fdfb1564e4fb04636d5e9cd23ff
李范红 <vivi3n@126.com>
2017-01-30 23:23:39 +0800

    my correction notebook

--- untitled0.py
+++ untitled0.py
@@ -5,12 +5,13 @@ Created on Wed Jan 25 00:30:48 2017
 
 @author: lifanhong
 """
-"""
+
 print('hello world')
 print('I like 6.00.1x')
 #in the following 3 expreesions, the word[-1]
 #repersents the last char of the string and
 #when the gap is -1 it starts at word[-1]
+
 print('')
 print('abcdefgh'[::-1])
 print('abcdefgh'[:-1:-1])
@@ -22,49 +23,27 @@ if x != 5:
     print('haha')
 else:
     print('kiki')
-    
-"""
 
-print("hello world")
+#我的小错题集
 
-if type(varA) == str or type(varB) == str:
-    print('string involved')
-elif varA > varB:
-    print('bigger')
-elif varA == varB:
-    print('equal')
-else:
-    print('smaller')
-    
-n = 2
-while n <= 10:
-    print(n)
-    n += 2
-print('Goodbye!')
+num = 10
+for num in range(5):
+    print(num)
+print(num)
 
-print('Hello!')
-n = 10
-while n >= 2:
-    print(n)
-    n -= 2
+#output 0, 1, 2, 3, 4, 4
 
-n = 1
-end = 6
-sum = 0
-while n <= end:
-    sum += n
-    n += 1
-print(sum)
+divisor = 2
+for num in range(0, 10, 2):
+    print(num/divisor) 
 
-for n in range(2,11,2):
-    print(n)
-print('Goodbye!')
+#output 0.0, 1.0, 2.0, 3.0, 4.0
 
-print('Hello!')
-for n in range(10,1,-2):
-    print(n)
-    
-sum = 0
-for n in range(1,7):
-    sum += n
-print(sum)
+for variable in range(20):
+    if variable % 4 == 0:
+        print(variable)
+    if variable % 16 == 0:
+        print('Foo!') 
+
+#output 0, 'Foo!', 4, 8, 12, 16, 'Foo!'
+
